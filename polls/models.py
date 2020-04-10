157e1dc693a97c7d94733fc4e94b384c7cdf621d
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-04-10 12:57:58 +0300

    added our models for the app's database

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Candidates(models.Model):
+    candidate_first_name = models.CharField(max_length=12)
+    candidate_middle_name = models.CharField(max_length=12)
+    candidate_last_name = models.CharField(max_length=12)
+    candidate_id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.candidate_first_name
+
+
+class Votes(models.Model):
+    voter_first_name = models.CharField(max_length=12)
+    voter_last_name = models.CharField(max_length=12)
+    voter_id = models.IntegerField(default=0)
+    voter_choice = models.CharField(max_length=30)
+    vote_submit_time = models.DateTimeField("date submitted")
+
+    def __str__(self):
+        return self.voter_choice
